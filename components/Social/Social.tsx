349fcaa4ea8821a55cc613396d6598afc0eacb33
Michael Showes <michael.showes@gmail.com>
2022-12-29 19:08:15 -0500

    added aria-labels to social buttons

--- components/Social/Social.tsx
+++ components/Social/Social.tsx
@@ -6,15 +6,15 @@ import Instagram from '../../assets/images/instagram.svg';
 export default function Social() {
   return (
     <div className={styles.social}>
-      <button className={styles.icon}>
+      <button className={styles.icon} aria-label='Facebook'>
         <Facebook />
       </button>
 
-      <button className={styles.icon}>
+      <button className={styles.icon} aria-label='Twitter'>
         <Twitter />
       </button>
 
-      <button className={styles.icon}>
+      <button className={styles.icon} aria-label='Instagram'>
         <Instagram />
       </button>
     </div>
